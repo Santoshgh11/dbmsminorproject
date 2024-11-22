d17643b5d9da3c743f18a94e80a90ae72bfbcd0d
Santoshgh11 <githubsantosh0@gmail.com>
2024-11-22 22:44:17 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (name, price, image_url) VALUES
+('Laptop', 70000, 'product1.jpeg'),
+('Laptop Bag', 1000, 'product2.jpeg'),
+('Mouse', 500, 'product3.jpeg'),
+('Laptop Camera', 2000, 'product4.jpeg'),
+('Keyboard', 3000, 'product5.jpeg'),
+('Pendrive', 800, 'product6.jpeg'),
+('Laptop Charger', 5000, 'product7.jpeg'),
+('Powerbank', 1500, 'product8.jpeg'),
+('Headphones', 1799, 'product9.jpeg'),
+('Laptop Stand', 399, 'product10.jpeg');
